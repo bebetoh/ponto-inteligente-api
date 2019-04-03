d0d003eba46dd0a11f35b69e5a35e320a407aa0f
bebetoh <bebetohlf@gmail.com>
2019-04-03 06:53:47 -0300

    criacao das entidades JPA

---
+++ src/main/java/com/beto/pontointeligente/api/entities/Empresa.java
@@ -0,0 +1,111 @@
+package com.beto.pontointeligente.api.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "empresa")
+public class Empresa implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2352454142015090869L;
+	
+	private Long id;
+	private String razaoSocial;
+	private String cnpj;
+	private Date dataCriacao;
+	private Date dataAtualizacao;
+	private List<Funcionario> funcionarios;
+
+	public Empresa() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	@Column(name = "razao_social", nullable = false)
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+	
+	@Column(name = "cnpj", nullable = false)
+	public String getCnpj() {
+		return cnpj;
+	}
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	
+	@Column(name = "data_criacao", nullable = false)
+	public Date getDataCriacao() {
+		return dataCriacao;
+	}
+	
+	public void setDataCriacao(Date dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+	
+	@Column(name = "data_atualizacao")
+	public Date getDataAtualizacao() {
+		return dataAtualizacao;
+	}
+	public void setDataAtualizacao(Date dataAtualizacao) {
+		this.dataAtualizacao = dataAtualizacao;
+	}
+
+	@OneToMany(mappedBy = "empresa", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	public List<Funcionario> getFuncionarios() {
+		return funcionarios;
+	}
+	public void setFuncionarios(List<Funcionario> funcionarios) {
+		this.funcionarios = funcionarios;
+	}
+	
+	@PrePersist
+	public void  prePersist() {
+		final Date atual = new Date();
+		this.dataAtualizacao = atual;
+		this.dataCriacao = atual;
+	}
+	
+	@PreUpdate
+	public void preUpdate() {
+		this.dataAtualizacao = new Date();
+	}
+
+	@Override
+	public String toString() {
+		return "Empresa [id=" + id + ", razaoSocial=" + razaoSocial + ", cnpj=" + cnpj + ", dataCriacao=" + dataCriacao
+				+ ", dataAtualizacao=" + dataAtualizacao + "]";
+	}
+
+	
+
+}
---
+++ src/main/java/com/beto/pontointeligente/api/enums/PerfilEnum.java
@@ -0,0 +1,6 @@
+package com.beto.pontointeligente.api.enums;
+
+public enum PerfilEnum {
+	ROLE_ADMIN,
+	ROLE_USUARIO;
+}
